4a6c0596e5d52af37ecf8954bd25185502c4d5f0
brunowang <625653507@qq.com>
2024-02-15 20:15:47 +0800

    encode and decode method for write ahead log

--- internal/data/wal.go
+++ internal/data/wal.go
@@ -2,21 +2,35 @@ package data
 
 import (
 	"encoding/binary"
+	"errors"
 	"hash/crc32"
 )
 
 const (
-	headerCrcSize    = 4
-	headerTypeSize   = 1
-	headerKeySize    = binary.MaxVarintLen32
-	headerValueSize  = binary.MaxVarintLen32
-	walHeaderMaxSize = headerCrcSize + headerTypeSize + headerKeySize + headerValueSize
+	headerCrcSize      = crc32.Size
+	headerTypeSize     = 1
+	headerKeyMaxSize   = binary.MaxVarintLen32
+	headerValueMaxSize = binary.MaxVarintLen32
+	walHeaderMaxSize   = headerCrcSize + headerTypeSize + headerKeyMaxSize + headerValueMaxSize
 )
 
+var (
+	ErrInvalidBytes = errors.New("invalid bytes value, wal maybe corrupted")
+	ErrInvalidCRC   = errors.New("invalid crc value, wal maybe corrupted")
+)
+
+type WALHeader struct {
+	CRC        uint32
+	IsDel      bool
+	KeySize    int
+	ValueSize  int
+	HeaderSize int
+}
+
 type WriteAheadLog struct {
+	WALHeader
 	Key   []byte
 	Value []byte
-	IsDel bool
 }
 
 func (l *WriteAheadLog) Encode() ([]byte, int) {
@@ -27,6 +41,7 @@ func (l *WriteAheadLog) Encode() ([]byte, int) {
 		isDelBit = 1
 	}
 	header[writeIdx] = isDelBit
+	writeIdx += headerTypeSize
 	writeIdx += binary.PutVarint(header[writeIdx:], int64(len(l.Key)))
 	writeIdx += binary.PutVarint(header[writeIdx:], int64(len(l.Value)))
 
@@ -42,4 +57,66 @@ func (l *WriteAheadLog) Encode() ([]byte, int) {
 	binary.LittleEndian.PutUint32(encBytes[:headerCrcSize], crc)
 
 	return encBytes, size
+}
+
+func (l *WriteAheadLog) DecodeHeader(bs []byte) error {
+	if len(bs) < headerCrcSize+headerTypeSize {
+		return ErrInvalidBytes
+	}
+	l.CRC = binary.LittleEndian.Uint32(bs[:headerCrcSize])
+	readIdx := headerCrcSize + headerTypeSize - 1
+
+	isDelBit := bs[readIdx]
+	if isDelBit == 1 {
+		l.IsDel = true
+	}
+	readIdx += headerTypeSize
+
+	keySize, n := binary.Varint(bs[readIdx:])
+	if n <= 0 {
+		return ErrInvalidBytes
+	}
+	l.KeySize = int(keySize)
+	readIdx += n
+
+	valueSize, n := binary.Varint(bs[readIdx:])
+	if n <= 0 {
+		return ErrInvalidBytes
+	}
+	l.ValueSize = int(valueSize)
+	readIdx += n
+
+	l.HeaderSize = readIdx
+	return nil
+}
+
+func (l *WriteAheadLog) DecodeBody(bs []byte) error {
+	if len(bs) < l.KeySize+l.ValueSize {
+		return ErrInvalidBytes
+	}
+	if l.KeySize > 0 {
+		l.Key = bs[:l.KeySize]
+	}
+	if l.ValueSize > 0 {
+		l.Value = bs[l.KeySize : l.KeySize+l.ValueSize]
+	}
+	return nil
+}
+
+func (l *WriteAheadLog) GetSize() int {
+	return l.HeaderSize + l.KeySize + l.ValueSize
+}
+
+func (l *WriteAheadLog) CheckCRC(headerBytes []byte) bool {
+	if l.HeaderSize < headerCrcSize {
+		return false
+	}
+	if len(headerBytes) < l.HeaderSize {
+		return false
+	}
+	crc := crc32.ChecksumIEEE(headerBytes[headerCrcSize:l.HeaderSize])
+	crc = crc32.Update(crc, crc32.IEEETable, l.Key)
+	crc = crc32.Update(crc, crc32.IEEETable, l.Value)
+
+	return crc == l.CRC
 }
--- internal/data/walfile.go
+++ internal/data/walfile.go
@@ -1,7 +1,6 @@
 package data
 
 import (
-	"errors"
 	"fmt"
 	"github.com/brunowang/gokv/internal/iomgr"
 	"path/filepath"
@@ -9,10 +8,6 @@ import (
 
 const (
 	WalFileSuffix = ".wal"
-)
-
-var (
-	ErrInvalidCRC = errors.New("invalid crc value, wal maybe corrupted")
 )
 
 type FilePos struct {
@@ -42,4 +37,43 @@ func OpenWalFile(dirPath string, fileID uint32) (*WalFile, error) {
 		},
 		ioMgr: fileMgr,
 	}, nil
+}
+
+func (f *WalFile) Read(offset int64) (*WriteAheadLog, error) {
+	fileSize, err := f.ioMgr.Size()
+	if err != nil {
+		return nil, err
+	}
+	walData := &WriteAheadLog{}
+
+	var headerSize int64 = walHeaderMaxSize
+	if offset+headerSize > fileSize {
+		headerSize = fileSize - offset
+	}
+
+	headerBytes := make([]byte, headerSize)
+	if _, err := f.ioMgr.Read(headerBytes, offset); err != nil {
+		return nil, err
+	}
+	if err := walData.DecodeHeader(headerBytes); err != nil {
+		return nil, err
+	}
+	offset += int64(walData.HeaderSize)
+
+	bodySize := walData.GetSize() - walData.HeaderSize
+
+	bodyBytes := make([]byte, bodySize)
+	if _, err := f.ioMgr.Read(bodyBytes, offset); err != nil {
+		return nil, err
+	}
+	if err := walData.DecodeBody(bodyBytes); err != nil {
+		return nil, err
+	}
+	offset += int64(bodySize)
+
+	if !walData.CheckCRC(headerBytes) {
+		return nil, ErrInvalidCRC
+	}
+
+	return walData, nil
 }
